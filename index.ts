c44690f9c869ff18fc8b2128d332ae9c54fb1281
Andy Olshansky <olshansa@gmail.com>
2024-09-22 19:55:26 -0400

    randomness introduced

--- index.ts
+++ index.ts
@@ -1,51 +1,56 @@
 console.log("Hello via Bun!");
 
 function runSingleThreaded(numTrials: number) {
-    let sum = 0;
-    const start = performance.now();
-    for (let i = 0; i < numTrials; i++) {
-        sum++;
+  const start = performance.now();
+  for (let i = 0; i < numTrials; i++) {
+    let num = 5;
+    while (num !== 0) {
+      num = Math.floor(Math.random() * 1000);
     }
-    const end = performance.now();
-    console.log(`${numTrials} single-threaded trials took ${end - start}ms`);
+  }
+  const end = performance.now();
+  console.log(`${numTrials} single-threaded trials took ${end - start}ms`);
 }
 
 function runMultiThreaded(numTrials: number, numWorkers: number) {
-    const trialsPerWorker = Math.floor(numTrials / numWorkers);
-    const workers = [];
-    let completedWorkers = 0;
-    let totalSum = 0;
+  const start = performance.now();
+  const trialsPerWorker = Math.floor(numTrials / numWorkers);
+  let completedWorkers = 0;
+  let workers = [];
 
-    const start = performance.now();
+  return new Promise<void>((resolve) => {
     for (let i = 0; i < numWorkers; i++) {
-        const worker = new Worker('./worker.ts');
+        const worker = new Worker(new URL('./worker.ts', import.meta.url));
         workers.push(worker);
     }
-
     for (const worker of workers) {
         worker.postMessage(trialsPerWorker);
 
-        worker.onmessage = (message) => {
-            totalSum += message.data;
+        worker.onmessage = () => {
             completedWorkers++;
-
             if (completedWorkers === numWorkers) {
                 const end = performance.now();
-                console.log(`${numTrials} single-threaded trials took ${end - start}ms`);
-                process.exit(0);
+                console.log(`${numTrials} multi-threaded trials took ${end - start}ms`);
+                workers.forEach((worker) => worker.terminate());
+                resolve();
             }
-        }
+        };
     }
+  });
 }
 
-if (Bun.argv.length > 2) {
+async function main() {
+    if (Bun.argv.length > 2) {
     const arg = Bun.argv[2];
-    const numTrials = 1000000000;
-    const numWorkers = 8;
+    const numTrials = 100000000;
+    const numWorkers = 32;
 
     if (arg === "single" || arg === "0") {
         runSingleThreaded(numTrials);
     } else if (arg === "multi" || arg === "1") {
         runMultiThreaded(numTrials, numWorkers);
     }
+  }
 }
+
+await main();
--- worker.ts
+++ worker.ts
@@ -1,10 +1,12 @@
-self.onmessage = (message) => {
-    const trials = message.data;
-    let sum = 0;
-
-    for (let i = 0; i < trials; i++) {
-        sum++;
+self.onmessage = (event) => {
+    const numTrials = event.data;
+    
+    for (let i = 0; i < numTrials; i++) {
+        let num = 5;
+        while (num !== 0) {
+            num = Math.floor(Math.random() * 1000);
+        }
     }
 
-    self.postMessage(sum);
+    self.postMessage('done');
 }
