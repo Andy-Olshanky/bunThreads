e490d08b62ac92757bcfe489ec983db8558102ec
Andy Olshansky <olshansa@gmail.com>
2024-09-22 19:00:29 -0400

    gonna multithread this

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 console.log("Hello via Bun!");
+
+const numTrials = 10000000000;
+let num = 0;
+const start = performance.now();
+
+for (let i = 0; i < numTrials; i++) {
+    num++;
+}
+
+const end = performance.now();
+console.log(`Trials: ${numTrials}, Sum: ${num}, Time: ${end - start}ms`);
